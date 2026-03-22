e7988ad33d5abdfa0c62904f45bfe80305efa01a
Kaio <trongleele@gmail.com>
2026-03-22 17:40:27 +0700

    update cors

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import express, { json } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import connect from '../configs/db/index.js';
 import routes from '../routes/index.js';
 
@@ -6,7 +8,28 @@ const app = express();
 
 connect();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://audio-king-fe.vercel.app',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Origin not allowed by CORS'));
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(json());
+app.use(cookieParser());
 
 routes(app);
 
