2bf81272f076d8353b85f2b40891fa4a43a49ec9
Kaio <trongleele@gmail.com>
2026-04-03 06:57:24 +0700

    update auth

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -7,22 +7,28 @@ dotenv.config();
 const { JWT_ACCESS_TOKEN } = process.env;
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.token;
-  if (token) {
-    const accessToken = token.split(' ')[1];
-    if (accessToken) {
-      jwt.verify(accessToken, JWT_ACCESS_TOKEN, (err, user) => {
-        if (err) {
-          return handleError403(res, 'Token không hợp lệ');
-        } else {
-          req.user = user;
-          next();
-        }
-      });
-    }
-  } else {
+  const authHeader = req.headers.authorization || req.headers.token;
+
+  if (!authHeader) {
     return handleError401(res, 'Bạn chưa đăng nhập');
   }
+
+  const accessToken = authHeader.startsWith('Bearer ')
+    ? authHeader.split(' ')[1]
+    : authHeader;
+
+  if (!accessToken) {
+    return handleError401(res, 'Bạn chưa đăng nhập');
+  }
+
+  jwt.verify(accessToken, JWT_ACCESS_TOKEN, (err, user) => {
+    if (err) {
+      return handleError403(res, 'Token không hợp lệ');
+    }
+
+    req.user = user;
+    next();
+  });
 };
 
 const verifyAuth = (req, res, next) => {
